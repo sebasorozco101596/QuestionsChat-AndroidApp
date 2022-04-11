ab8fd8239ccf79f3b8078f80ce7c80881b7fa769
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-04-11 17:18:50 -0400

    Adding the activity chat and doing some changes of the style for to add the messages that will be sending and receiving.

---
+++ app/src/main/java/com/sebasorozcob/www/questionschat/view/activities/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.sebasorozcob.www.questionschat.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sebasorozcob.www.questionschat.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
